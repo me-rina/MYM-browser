7b01782d598166a406cc5f5fc7caa5eabf92f151
me-rina <amr@outsidethepond.com>
2015-01-13 13:12:49 -0500

    vs 0.4
    game funs working. need more drawing. argh

--- js/playMYM.js
+++ js/playMYM.js
@@ -8,11 +8,16 @@
 
 /* so to speak, globals */
 
-var arr_len = 4; // length of input array
+var num_pegs = 4; // length of input array
+var num_guesses = num_pegs * 3
 var game_colors = {
   "pot": ["red","blue","green","yellow"],
   "potHex": ["#ff2402",'#0c00ff','#05ff3b', '#ffff05']
   }
+var color_key ={"r": "red","b": "blue","g": "green","y": "yellow"}
+
+var game_state = {"w": "YOU WIN!", "l": "YOU LOSE!"}
+
 var moves = []
 var results = []
 
@@ -37,26 +42,93 @@ function random_array( num ) {
 }
 function set_active_color(color){
 
- var c1 = document.getElementById("activeColor");
+ var c1 = document.getElementById("playingField");
     var ctx = c1.getContext("2d");
     ctx.fillStyle = color;
-    ctx.fillRect(40,4,40,40);
-	ctx.strokeRect(38,2,44,44);
+    ctx.fillRect(36,355,40,40);
+	ctx.strokeRect(34,353,44,44);
 
 	}
+	
+function declare_game_over(state) {
+    var c1 = document.getElementById("playingField");
+    var ctx = c1.getContext("2d");
+	ctx.fillStyle="yellow"
+	ctx.fillRect(480,30,120,20);
+	ctx.font="20px Verdana";
+	ctx.fillStyle="red";
+    ctx.fillText(game_state[state],480,50);
+	}
+
 
 function set_goal(thisOrder) {
         var goal=[];
-    for (var i=0;i< arr_len;i++) {
+    for (var i=0;i< num_pegs;i++) {
 		    goal.push(game_colors["pot"][thisOrder[i]]);
 		}
     return goal
 	}	
+	
+function eval_move(movex) {
+    /* contract: array (the move) -> array (the result) */
+    var exact_match = 0;
+	var color_match = 0;
+    var g_rest = [];
+    var m_rest = [];
+	var this_result=[];
+	
+	/* check for exact match */
+	for (var i in movex) {
+    	if (movex[i] == theGoal[i]) {this_result.push("T");}
+		else {g_rest.push(theGoal[i]);m_rest.push(movex[i]);}
+	    }
+	
+    /* now check for color match 2 loops */	
+	g_rest=g_rest.sort();m_rest=m_rest.sort();
+	for (i in m_rest) {
+	    if (m_rest[i] == g_rest[i]) {this_result.push("t");}
+	    
+		   }
+    
+    
+	for (i = this_result.length;i<num_pegs;i++) {this_result.push("-");}
+	
+	return this_result
+	
+}
+
+function make_move(movex) {
+    moves.push(movex);
+	results.push(eval_move(movex));
+	is_game_over();
+	}
+	
+function is_game_over() {
+    if (results[results.length-1].toString() ==	"T,T,T,T" ) {
+	/* you win , print message, end game */
+	declare_game_over("w");
+	}
+	else {if (results.length == num_guesses) {
+	declare_game_over("l");
+	}
+	}
+}	
+	
+    	
+function str_to_move(str) {
+   var newarr=[];
+   for (var i=0;i<str.length;i++) {
+   newarr.push(color_key[str.substr(i,1)]);
+   }
+   return newarr
+   }
+
+    	
 /* __mainloop__ */
 
 set_active_color("#f4f4f4");
 
-var theGoal = set_goal(random_array(arr_len));
+var theGoal = set_goal(random_array(num_pegs));
 
 document.getElementsByClassName("pot")
 
